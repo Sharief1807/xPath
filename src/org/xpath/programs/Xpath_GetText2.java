902d5aa6af8167285ae6219c1e2022a4b001d9b9
Sharief1807 <sharief.indian@gmail.com>
2019-04-04 12:14:49 +0530

    resolved

--- src/org/xpath/programs/Xpath_GetText2.java
+++ src/org/xpath/programs/Xpath_GetText2.java
@@ -33,8 +33,6 @@ public class Xpath_GetText2 {
 		System.out.println(str7);
 		System.out.println(str8);
 		System.out.println(str9);
-		System.out.println("endddddd");
-		System.out.println("stmttttt");
 	}
 
 }
